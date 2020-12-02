71be727d735f9aa7be09df2867b9661c64f91630
bhshp <shp_123@foxmail.com>
2020-12-02 21:54:09 +0800

    finish iterable container direct std::cout

--- demo/ostream.cc
+++ demo/ostream.cc
@@ -1,7 +1,18 @@
+#include <map>
+#include <set>
+#include <vector>
+
 #include "ostream_util.h"
 
 int main() {
+    std::cout << std::boolalpha << is_iterable<std::string>{}() << std::endl;
     std::tuple<int, int, int, std::string> tuple{2020, 11, 9, "14:18"};
     std::cout << tuple << std::endl;
+    std::vector<int> v{1, 2, 3, 5, 8};
+    std::cout << v << std::endl;
+    std::map<int, std::pair<int, double>> map{{1, {1, 2.0}}};
+    std::cout << map << std::endl;
+    std::set<int> set{1, 2, 3, 5, 8};
+    std::cout << set << std::endl;
     return 0;
 }
--- include/ostream_util.h
+++ include/ostream_util.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <iterator>
 #include <tuple>
 #include <type_traits>
 #include <utility>
 
+/**
+ * output pair<T, S>, tuple<A, B, ...>, container that supports iterator begin, end.
+ */
+
 // declaration
+template <typename T>
+struct is_iterable;
 
 template <typename T, typename S>
 constexpr std::ostream &
@@ -18,6 +25,14 @@ operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept;
 template <size_t n = 0, typename... Args>
 constexpr typename std::enable_if_t<(n < sizeof...(Args)), std::ostream &>
 operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept;
+
+template <typename Container>
+constexpr typename std::enable_if_t<is_iterable<Container>{}(), std::ostream &>
+operator<<(std::ostream &os, const Container &v) noexcept;
+
+template <typename Container>
+constexpr typename std::enable_if_t<is_iterable<Container>{}(), std::ostream &>
+ostream_container_impl(std::ostream &os, const Container &v, typename Container::const_iterator cit) noexcept;
 
 // implementation
 
@@ -37,4 +52,28 @@ template <size_t n, typename... Args>
 constexpr typename std::enable_if_t<(n < sizeof...(Args)), std::ostream &>
 operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept {
     return operator<<<n + 1, Args...>(os << (n ? "" : "<") << std::get<n>(t) << (n + 1 == sizeof...(Args) ? '>' : ','), t);
+}
+
+template <typename T>
+struct is_iterable {
+   public:
+    constexpr bool operator()() noexcept { return value; }
+
+   private:
+    template <typename Tp>
+    constexpr static std::true_type exist_type(typename Tp::const_iterator *, decltype(&Tp::cbegin), decltype(&Tp::cend)) noexcept;
+    template <typename Tp>
+    constexpr static std::false_type exist_type(...) noexcept;
+    using exist = decltype(exist_type<T>(nullptr, nullptr, nullptr));
+    constexpr static bool value = std::is_same<exist, std::true_type>{}() && !std::is_same<T, std::string>{}();
+};
+
+template <typename Container>
+constexpr typename std::enable_if_t<is_iterable<Container>{}(), std::ostream &> ostream_container_impl(std::ostream &os, const Container &v, typename Container::const_iterator cit) noexcept {
+    return cit == std::cend(v) ? os : ostream_container_impl(os << *cit << (++cit == std::cend(v) ? "" : ","), v, cit);
+}
+
+template <typename Container>
+constexpr typename std::enable_if_t<is_iterable<Container>{}(), std::ostream &> operator<<(std::ostream &os, const Container &v) noexcept {
+    return ostream_container_impl(os << '{', v, std::cbegin(v)) << '}';
 }
