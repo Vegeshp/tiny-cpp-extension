e550dcd2b96747f2ebf593722332609a2836508b
bhshp <shp_123@foxmail.com>
2020-12-02 23:47:28 +0800

    densify code

--- include/ostream_util.h
+++ include/ostream_util.h
@@ -15,44 +15,32 @@ template <typename T>
 struct ostream_operator_overridable;
 
 template <typename T, typename S>
-constexpr std::ostream &
-operator<<(std::ostream &os, const std::pair<T, S> &p) noexcept;
+constexpr std::ostream &operator<<(std::ostream &os, const std::pair<T, S> &p) noexcept;
 
 template <size_t n = 0, typename... Args>
-constexpr typename std::enable_if_t<(n >= sizeof...(Args)), std::ostream &>
-operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept;
+constexpr typename std::enable_if_t<(n >= sizeof...(Args)), std::ostream &> operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept;
 
 template <size_t n = 0, typename... Args>
-constexpr typename std::enable_if_t<(n < sizeof...(Args)), std::ostream &>
-operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept;
+constexpr typename std::enable_if_t<(n < sizeof...(Args)), std::ostream &> operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept;
 
 template <typename Container>
-constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &>
-operator<<(std::ostream &os, const Container &v) noexcept;
+constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &> operator<<(std::ostream &os, const Container &v) noexcept;
 
 template <typename Container>
-constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &>
-ostream_container_impl(std::ostream &os, const Container &v, typename Container::const_iterator cit) noexcept;
+constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &> ostream_container_impl(std::ostream &os, const Container &v, typename Container::const_iterator cit) noexcept;
 
 // implementation
 
 template <typename T, typename S>
-constexpr std::ostream &
-operator<<(std::ostream &os, const std::pair<T, S> &p) noexcept {
-    return os << '<' << p.first << ',' << p.second << '>';
-}
+constexpr std::ostream &operator<<(std::ostream &os, const std::pair<T, S> &p) noexcept { return os << '<' << p.first << ',' << p.second << '>'; }
 
 template <size_t n, typename... Args>
 constexpr typename std::enable_if_t<(n >= sizeof...(Args)), std::ostream &>
-operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept {
-    return os;
-}
+operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept { return os; }
 
 template <size_t n, typename... Args>
 constexpr typename std::enable_if_t<(n < sizeof...(Args)), std::ostream &>
-operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept {
-    return operator<<<n + 1, Args...>(os << (n ? "" : "<") << std::get<n>(t) << (n + 1 == sizeof...(Args) ? '>' : ','), t);
-}
+operator<<(std::ostream &os, const std::tuple<Args...> &t) noexcept { return operator<<<n + 1, Args...>(os << (n ? "" : "<") << std::get<n>(t) << (n + 1 == sizeof...(Args) ? '>' : ','), t); }
 
 template <typename T>
 struct ostream_operator_overridable {
@@ -77,11 +65,7 @@ struct ostream_operator_overridable {
 };
 
 template <typename Container>
-constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &> ostream_container_impl(std::ostream &os, const Container &v, typename Container::const_iterator cit) noexcept {
-    return cit == std::cend(v) ? os : ostream_container_impl(os << *cit << (++cit == std::cend(v) ? "" : ","), v, cit);
-}
+constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &> ostream_container_impl(std::ostream &os, const Container &v, typename Container::const_iterator cit) noexcept { return cit == std::cend(v) ? os : ostream_container_impl(os << *cit << (++cit == std::cend(v) ? "" : ","), v, cit); }
 
 template <typename Container>
-constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &> operator<<(std::ostream &os, const Container &v) noexcept {
-    return ostream_container_impl(os << '{', v, std::cbegin(v)) << '}';
-}
+constexpr typename std::enable_if_t<ostream_operator_overridable<Container>{}(), std::ostream &> operator<<(std::ostream &os, const Container &v) noexcept { return ostream_container_impl(os << '{', v, std::cbegin(v)) << '}'; }
