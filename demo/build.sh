1fbd695d9f12eab0552b499c97601016dcfcce0a
bhshp <shp_123@foxmail.com>
2020-12-04 09:32:09 +0800

    fix build script

--- demo/build.sh
+++ demo/build.sh
@@ -1,8 +1,12 @@
 cc=clang++
 out=a.out
+
 # $cc -c ./../src/time_util.cc -I ./../include/ -O2 -std=c++14 -o libtime_util.a
 # $cc main.cc -L ./ -I ./../include/ -ltime_util -o a.out
 # rm *.a *.out
+
 $cc ./../src/time_util.cc main.cc -I ./../include -O2 -std=c++14 -o $out
+./$out
+$cc ostream.cc -I ./../include -O2 -std=c++14 -o $out
 ./$out
 rm *.out
