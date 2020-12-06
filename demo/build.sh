2f912fe823c078548043b5aa315a0edcb8607b71
bhshp <shp_123@foxmail.com>
2020-12-06 21:34:55 +0800

    add line seperator

--- demo/build.sh
+++ demo/build.sh
@@ -3,11 +3,11 @@ out=a.out
 
 $cc ./../src/time_util.cc time.cc -I ./../include -O2 -std=c++14 -o $out
 ./$out
-
+printf '\n'
 $cc ostream.cc -I ./../include -O2 -std=c++14 -o $out
 ./$out
-
+printf '\n'
 $cc random.cc -I ./../include -O2 -std=c++14 -o $out
 ./$out
-
+printf '\n'
 rm *.out
